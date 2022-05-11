f1ab29ab519971015e9aea0c2ab0c94806c35835
highonskooma <joaoguireis99@gmail.com>
2022-05-11 20:27:35 +0100

    react - adicionar utilizador

--- src/webapp/src/App.js
+++ src/webapp/src/App.js
@@ -17,6 +17,29 @@ function App() {
         >
           Learn React
         </a>
+
+          <form action="/adduser" method="POST">
+            <p>Vamos adicionar um utilizador</p>
+            <div>
+              <label htmlFor="firstName">Nome próprio:</label> <br/>
+              <input name="first" id="firstName" defaultValue="Homer"/>
+            </div>
+            <div>
+              <label htmlFor="lastName">Apelido:</label> <br/>
+              <input name="last" id="lastName" defaultValue="Simpson"/>
+            </div>
+            <div>
+              <label htmlFor="codigo">Número de funcionário:</label> <br/>
+              <input name="codigo" id="codigo" defaultValue="F"/>
+            </div>
+            <div>
+              <button>Adicionar Utilizador!</button>
+              <br/>
+            </div>
+          </form>
+        <form action="/listuser" method="GET">
+          <button>Listar utilizador</button>
+        </form>
       </header>
     </div>
   );
