f45bbee277519f9f4c3152441048a3a76928d563
highonskooma <joaoguireis99@gmail.com>
2022-05-31 16:51:14 +0100

    log in - log out

--- src/webapp/src/App.js
+++ src/webapp/src/App.js
@@ -30,8 +30,9 @@ function Home() {
             </ul>
 
             <div className="col-md-3 text-end" >
-                <button type="button" className="btn btn-outline-primary me-2">Login</button>
-                <button type="button" className="btn btn-primary">Registo</button>
+                <a href="logout">
+                    <button type="button" className="btn btn-outline-primary me-2">Logout</button>
+                </a>
             </div>
         </header>
 
