74e3ec80e687c58a7f30ed244c95669ec5bb4e9f
JohnnyCines <johnycvpmarques@icloud.com>
2022-06-04 01:29:11 +0100

    Reservas "completas"

--- src/webapp/src/Reservas.js
+++ src/webapp/src/Reservas.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import React from "react";
 import {Link} from "react-router-dom";
 
+//local final?
 function Reservas(){
     return(
         <div className="Reservas">
@@ -29,60 +29,91 @@ function Reservas(){
                     <button type="button" className="btn btn-outline-primary me-2">Logout</button>
                 </div>
             </header>
+
             <div className="reservation_container">
-                <div className="col-md-7 col-lg-8">
+                <div className="centered">
                     <div className="col-md-7 col-lg-8">
                         <h4 className="mb-3">Faça a sua reserva</h4>
-                        <form className="needs-validation" noValidate>
                             <div className="row g-3">
-                                <div className="col-12">
-                                    <label htmlFor="username" className="form-label">Nome de Utilizador</label>
-                                    <div className="input-group has-validation">
-                                        <input type="text" className="form-control" id="username" placeholder="Nome de Utilizador"
+                                <form action="/addtrip" method="POST" id="trip-form">
+
+                                    <div className="col-12">
+                                        <label htmlFor="address" className="form-label">Morada Destino</label>
+                                        <input name="local_i" type="text" className="form-control" id="address" placeholder="1234 Main St"
                                                required/>
-                                            <div className="invalid-feedback">
-                                                Insira o seu nome de Utilizador.
-                                            </div>
-                                    </div>
-                                </div>
-
-                                <div className="col-12">
-                                    <label htmlFor="address" className="form-label">Destino</label>
-                                    <input type="text" className="form-control" id="address" placeholder="Rua de São Brás 12"
-                                           required/>
                                         <div className="invalid-feedback">
-                                            Insira a morada de Destino.
+                                            Por favor insira a morada de destino.
                                         </div>
-                                </div>
-
-                                <div className="col-md-4">
-                                    <label htmlFor="state" className="form-label">Cidade</label>
-                                    <select className="form-select" id="state" required>
-                                        <option value="">Escolha...</option>
-                                        <option>Braga</option>
-                                        <option>Bragança</option>
-                                        <option>Vila do Conde</option>
-                                        <option>Viana</option>
-                                        <option>Vizela</option>
-                                    </select>
-                                    <div className="invalid-feedback">
-                                        Insira a cidade de Destino.
                                     </div>
-                                </div>
 
-                                <div className="col-md-3">
-                                    <label htmlFor="zip" className="form-label">Zip</label>
-                                    <input type="text" className="form-control" id="Código Postal" placeholder="____-___" required/>
+                                    <div className="col-12">
+                                        <label htmlFor="addressfinal" className="form-label">Morada Final</label>
+                                        <input name="local_f" type="text" className="form-control" id="addressfinal" placeholder="1234 Main St"
+                                               required/>
                                         <div className="invalid-feedback">
-                                            Insira o código postal de Destino.
+                                            Por favor insira a morada final.
                                         </div>
-                                </div>
+                                    </div>
+
+                                    <div className="col-12">
+                                        <label htmlFor="dateinit" className="form-label">Data de inicio</label>
+                                        <input name="start" type="text" className="form-control" id="dateinit" placeholder="dd/mm/yy hh:mm:ss"
+                                               required/>
+                                        <div className="invalid-feedback">
+                                            Por favor insira a data.
+                                        </div>
+                                    </div>
+
+                                    <div className="col-12">
+                                        <label htmlFor="datefinal" className="form-label">Data de final</label>
+                                        <input name="finish" type="text" className="form-control" id="datefinal" placeholder="dd/mm/yy hh:mm:ss"
+                                               required/>
+                                        <div className="invalid-feedback">
+                                            Por favor insira a data.
+                                        </div>
+                                    </div>
+
+                                    <div className="col-12">
+                                        <label htmlFor="passagers" className="form-label">Passageiros</label>
+                                        <input name="passageiros" type="text" className="form-control" id="passagers" placeholder="Insira o número de passageiros"
+                                               required/>
+                                        <div className="invalid-feedback">
+                                            Número Inválido.
+                                        </div>
+                                    </div>
+
+                                    <div className="col-12">
+                                        <label htmlFor="kmsi" className="form-label">Kms iniciais</label>
+                                        <input name="kms_i" type="text" className="form-control" id="kmsi" placeholder="Insira o número de kms inicial do veículo"
+                                               required/>
+                                        <div className="invalid-feedback">
+                                            Por favor insira o número de quilómetros
+                                        </div>
+                                    </div>
+
+                                    <div className="col-12">
+                                        <label htmlFor="kmsf" className="form-label">Kms iniciais</label>
+                                        <input name="kms_f" type="text" className="form-control" id="kmsf" placeholder="Insira o número de kms final do veículo"
+                                               required/>
+                                        <div className="invalid-feedback">
+                                            Por favor insira o número de quilómetros
+                                        </div>
+                                    </div>
+
+                                    <div className="col-12">
+                                        <label htmlFor="vehicle" className="form-label">Id do veículo</label>
+                                        <input name="veiculo" type="text" className="form-control" id="vehicle" placeholder="Insira o id do veículo"
+                                               required/>
+                                        <div className="invalid-feedback">
+                                            Por favor insira o id.
+                                        </div>
+                                    </div>
+
+                                    <button className="w-100 btn btn-primary btn-lg" type="submit">Validar</button>
+
+
+                                </form>
                             </div>
-
-                                <hr className="my-4"/>
-                                        <button className="w-100 btn btn-primary btn-lg" type="submit">Validar
-                                        </button>
-                        </form>
                     </div>
                 </div>
             </div>
