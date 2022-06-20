967a3a627c979f0ac5861b9568d4f7413ac32446
highonskooma <joaoguireis99@gmail.com>
2022-06-21 00:13:45 +0100

    funções para modificar o server ldap

--- src/main/java/com/example/uniauto/WebSecurityConfig.java
+++ src/main/java/com/example/uniauto/WebSecurityConfig.java
@@ -3,10 +3,12 @@ package com.example.uniauto;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+@EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
