ef2240dd7cbc3a02ba6c4eeab9a6e04781abc4e2
JohnnyCines <johnycvpmarques@icloud.com>
2022-06-28 15:28:46 +0100

    Logins a funcionar

--- src/webapp/src/DataList.js
+++ src/webapp/src/DataList.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Button, ButtonGroup, Container, Table} from 'reactstrap';
+import {Link} from "react-router-dom";
 
 class DataList extends Component {
     constructor(props) {
@@ -56,10 +57,123 @@ class DataList extends Component {
         const {users, trips, available, cars, log} = this.state;
 
         if (this.props.statement==='log') {
+            if(log.firstName === "Gestor"){
+                return(
+                    <header
+                        className="shadow-sm d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-0 border-bottom">
+                        <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                            <h1 id="uni"> uni </h1>
+                            <h1 id="auto">Auto </h1>
+                        </a>
+
+                        <nav className="navbar">
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="#">Utilizadores</a>
+                                    <ul>
+                                        <li><Link to="/ListaUtilizadores" className="nav-link px-2 link-dark">Lista de
+                                            Utilizadores</Link></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Reservas</a>
+                                    <ul>
+                                        <li><Link to="/adicionaReservas" className="nav-link px-2 link-dark">Adiciona
+                                            Reserva</Link></li>
+                                        <li><Link to="/ListaReservas" className="nav-link px-2 link-dark">Lista de
+                                            Viagens</Link></li>
+                                        <li><Link to="/fazerCheckout" className="nav-link px-2 link-dark">Faz Checkout da
+                                            Viagem</Link></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Veiculos</a>
+                                    <ul>
+                                        <li><Link to="/veiculos" className="nav-link px-2 link-dark">Adiciona Veículo</Link>
+                                        </li>
+                                        <li><Link to="/ListaVeiculos" className="nav-link px-2 link-dark">Lista de
+                                            Veículos</Link></li>
+                                    </ul>
+                                </li>
+                                <li><Link to="/Sobre" className="nav-link px-2 link-dark">Sobre</Link></li>
+                            </ul>
+                        </nav>
+                    </header>
+                )
+            }
+
+            if(log.firstName === ""){
+                return (
+                    <header
+                        className="shadow-sm d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-0 border-bottom">
+                        <a href="/"
+                           className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                            <h1 id="uni"> uni </h1>
+                            <h1 id="auto">Auto </h1>
+                        </a>
+
+                        <nav className="navbar">
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="#">Utilizadores</a>
+                                    <ul>
+                                        <li><Link to="/Utilizadores" className="nav-link px-2 link-dark">Adiciona
+                                            Utilizador</Link></li>
+                                        <li><Link to="/ListaUtilizadores" className="nav-link px-2 link-dark">Lista de
+                                            Utilizadores</Link></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Reservas</a>
+                                    <ul>
+                                        <li><Link to="/adicionaReservas" className="nav-link px-2 link-dark">Adiciona
+                                            Reserva</Link></li>
+                                        <li><Link to="/ListaReservas" className="nav-link px-2 link-dark">Lista de
+                                            Viagens</Link></li>
+                                        <li><Link to="/fazerCheckout" className="nav-link px-2 link-dark">Faz Checkout
+                                            da Viagem</Link></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Veiculos</a>
+                                    <ul>
+                                        <li><Link to="/veiculos" className="nav-link px-2 link-dark">Adiciona
+                                            Veículo</Link></li>
+                                        <li><Link to="/ListaVeiculos" className="nav-link px-2 link-dark">Lista de
+                                            Veículos</Link></li>
+                                    </ul>
+                                </li>
+                                <li><Link to="/Sobre" className="nav-link px-2 link-dark">Sobre</Link></li>
+                            </ul>
+                        </nav>
+                    </header>
+                )
+            }
             return(
-                <div>
-                    {log.firstName}
-                </div>
+                <header
+                    className="shadow-sm d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-0 border-bottom">
+                    <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                        <h1 id="uni"> uni </h1>
+                        <h1 id="auto">Auto </h1>
+                    </a>
+
+                    <nav className="navbar">
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="#">Reservas</a>
+                                <ul>
+                                    <li><Link to="/adicionaReservas" className="nav-link px-2 link-dark">Adiciona
+                                        Reserva</Link></li>
+                                    <li><Link to="/fazerCheckout" className="nav-link px-2 link-dark">Faz Checkout da
+                                        Viagem</Link></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Veiculos</a>
+                                <ul>
+                                    <li><Link to="/ListaVeiculos" className="nav-link px-2 link-dark">Lista de
+                                        Veículos</Link></li>
+                                </ul>
+                            </li>
+                            <li><Link to="/Sobre" className="nav-link px-2 link-dark">Sobre</Link></li>
+                        </ul>
+                    </nav>
+                </header>
             )
         }
 
